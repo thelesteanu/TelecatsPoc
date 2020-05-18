07cfba05a7db6f740e085c97fbcd12c79e2c6036
Helesteanu Tudor <helesteanu.tudor@yahoo.ro>
2020-05-18 10:57:10 +0300

    Create poc for telecats app

--- src/main/java/com/webhelp/Application.java
+++ src/main/java/com/webhelp/Application.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import javax.swing.*;
+
 @SpringBootApplication
-public class Application {
+public class Application{
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class).headless(false).run(args);
-        MainMenuController mainMenuController = context.getBean(MainMenuController.class);
-        mainMenuController.prepareAndOpenFrame();
+        SwingUtilities.invokeLater(() -> {
+            MainMenuController mainMenuController = context.getBean(MainMenuController.class);
+            mainMenuController.prepareAndOpenFrame();
+        });
+
     }
 }
--- src/main/java/com/webhelp/ui/login/controller/MainMenuController.java
+++ src/main/java/com/webhelp/ui/login/controller/MainMenuController.java
@@ -1,7 +1,6 @@
 package com.webhelp.ui.login.controller;
 
 import com.webhelp.ui.login.view.MainMenuFrame;
-import com.webhelp.ui.login.view.RecordingControlFrame;
 import com.webhelp.ui.shared.controller.AbstractFrameController;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +9,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Log4j2
 public class MainMenuController extends AbstractFrameController {
-
     private final MainMenuFrame mainMenuFrame;
+
     private final RecordingController recordingController;
+
     @Autowired
-    public MainMenuController(MainMenuFrame mainMenuFrame,
-                              RecordingController recordingController) {
+    public MainMenuController(MainMenuFrame mainMenuFrame, RecordingController recordingController) {
         this.mainMenuFrame = mainMenuFrame;
         this.recordingController = recordingController;
     }
 
+
     public void prepareAndOpenFrame() {
+
         registerAction(mainMenuFrame.getLoginButton(), (e) -> {
             //Call login Api
             openRecordingWindow(1950000L);
